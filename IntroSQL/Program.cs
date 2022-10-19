d08a93453335cd7695fcae33f4c87fcb1b604da6
Carlos Reynaga <carandreyn@gmail.com>
2022-10-19 08:41:37 -0700

    edited git ignore

---
+++ IntroSQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
